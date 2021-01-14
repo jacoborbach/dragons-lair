ac52ef18201b6bbf352bc32e7a662175b4dc21ef
jacoborbach <jacoborbach96@gmail.com>
2021-01-14 16:18:12 -0500

    second commit

--- server/index.js
+++ server/index.js
@@ -1,26 +1,30 @@
+//requirements
 require('dotenv').config();
-
 const express = require('express');
-const expressSession = require('express-session');
+const session = require('express-session');
 const massive = require('massive');
-
 const { CONNECTION_STRING, SESSION_SECRET } = process.env;
-
 const PORT = 4000;
 const app = express();
+//---------------------------------------------------------------------
 
 app.use(express.json())
 
+//connect database
 massive({
-    connectionString = CONNECTION_STRING,
+    connectionString: CONNECTION_STRING,
     ssl: { rejectUnauthorized: false }
 }).then(db => {
     app.set('db', db);
     console.log('db connected');
 })
 
-
-
-
+//set up session?
+app.use(session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 180 }
+}));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`))
